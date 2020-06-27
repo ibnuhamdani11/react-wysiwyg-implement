c4124259cb049aa0c597201430db2ec227feafd8
Ardhi <ipanfifanfi@gmail.com>
2020-06-27 18:45:14 +0700

    change using ckeditor

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React, { Component } from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import Table from '@ckeditor/ckeditor5-table/src/table';
+// import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+// import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+// import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
 
-class App extends React.Component {
- handleEditorChange = (content, editor) => {
-   console.log('Content was updated:', content);
- }
-
- render() {
-   return (
-    <Editor
-      initialValue="<p>This is the initial content of the editor</p>"
-      apiKey="fbfv61ea1truzo55ewqwsakzgigwpmdhkw03iqerg6brt5jr"
-      init={{
-         height: 500,
-         menubar: true,
-         plugins: [
-           'advlist autolink lists link image charmap print preview anchor',
-           'searchreplace visualblocks code fullscreen',
-           'insertdatetime media table paste code help wordcount'
-         ],
-         toolbar:
-           'undo redo | formatselect | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           table | \
-           bullist numlist outdent indent | removeformat | help'
-       }}
-       onEditorChange={this.handleEditorChange}
-     />
-   );
- }
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onInit={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
