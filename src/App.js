3f96aba8c64080b565542faf1c940c48b131b101
Ardhi <ipanfifanfi@gmail.com>
2020-07-03 00:46:23 +0700

    adding header footer

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
 import React, { Component } from 'react';
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-// import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-// import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
-// import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
-
-// import Table from '@ckeditor/ckeditor5-table/src/table';
-
-const editorConfiguration = {
-    // plugins: [ TableToolbar, TableProperties, TableCellProperties ]
-    // toolbar: [ 'insertTable' ],
-
-    table: {
-        contentToolbar: [
-            'tableColumn', 'tableRow', 'mergeTableCells',
-            'tableProperties', 'tableCellProperties'
-        ],
-
-        // Configuration of the TableProperties plugin.
-        tableProperties: {
-            // ...
-        },
-
-        // Configuration of the TableCellProperties plugin.
-        tableCellProperties: {
-            // ...
-        }
-    }
-};
+// import ReactRichEditor from 'react-rich-text-editor'
+import TinymceExample from './TinymceExample.js'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    config={ editorConfiguration }
-                    // table={}
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onInit={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
-    }
+            <TinymceExample />
+        )
+    }    
 }
 
 export default App;
--- src/TinymceExample.js
+++ src/TinymceExample.js
@@ -1,32 +1,78 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import jsPDF from 'jspdf';
+import header from './assets/images/header.PNG';
+import footer from './assets/images/footer.PNG';
+
+console.log(header);
+
 
 class App extends React.Component {
  handleEditorChange = (content, editor) => {
    console.log('Content was updated:', content);
  }
 
+ printToPdf = () => {
+  // alert('hi');
+  // window.print();
+  // var doc = new jsPDF();
+  // doc.text(10, 10, 'This is a test');
+  // doc.autoPrint({variant: 'non-conform'});
+  // doc.save('autoprint.pdf');
+
+  // var doc = new jsPDF();          
+  // var elementHandler = {
+  //   '#ignorePDF': function (element, renderer) {
+  //     return true;
+  //   }
+  // };
+  // var source = window.document.getElementsByTagName("body")[0];
+  // var source = window.document.getElementById("tinymce")
+  // var source = document.querySelector("iframe")
+  // doc.fromHTML(
+  //     source,
+  //     15,
+  //     15,
+  //     {
+  //       'width': 180,
+  //       // 'elementHandlers': elementHandler
+  //     }
+  //     );
+
+  // doc.output("dataurlnewwindow");
+
+  // document.querySelector("iframe").contentWindow.print()
+  document.querySelector("#header_doc").contentWindow.print();
+
+ }
+
  render() {
    return (
-    <Editor
-      initialValue="<p>This is the initial content of the editor</p>"
-      apiKey="fbfv61ea1truzo55ewqwsakzgigwpmdhkw03iqerg6brt5jr"
-      init={{
-         height: 500,
-         menubar: true,
-         plugins: [
-           'advlist autolink lists link image charmap print preview anchor',
-           'searchreplace visualblocks code fullscreen',
-           'insertdatetime media table paste code help wordcount'
-         ],
-         toolbar:
-           'undo redo | formatselect | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           table | \
-           bullist numlist outdent indent | removeformat | help'
-       }}
-       onEditorChange={this.handleEditorChange}
-     />
+    <Fragment>
+      <Editor
+        initialValue="<p>This is the initial content of the editor</p>"
+        apiKey="fbfv61ea1truzo55ewqwsakzgigwpmdhkw03iqerg6brt5jr"
+        init={{
+           height: 500,
+           menubar: true,
+           valid_elements : '*[*]',
+           valid_styles: '*[*]',
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code fullscreen',
+             'insertdatetime media table paste code help wordcount print'
+           ],
+           toolbar:
+             'undo redo | formatselect | bold italic backcolor | \
+             alignleft aligncenter alignright alignjustify | \
+             table | \
+             bullist numlist outdent indent | removeformat | help | print'
+         }}
+         onEditorChange={this.handleEditorChange}
+       />
+       <img id="header_doc" scr={header} />
+       <button onClick={() => this.printToPdf() }>Print</button>
+     </Fragment>
    );
  }
 }
