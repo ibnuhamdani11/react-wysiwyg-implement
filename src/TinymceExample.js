24f6f50316726f79899b78ff6ee6d69702de272f
Ardhi <ipanfifanfi@gmail.com>
2020-07-04 15:36:55 +0700

    add header and footer

--- src/TinymceExample.js
+++ src/TinymceExample.js
@@ -3,9 +3,7 @@ import { Editor } from '@tinymce/tinymce-react';
 import jsPDF from 'jspdf';
 import header from './assets/images/header.PNG';
 import footer from './assets/images/footer.PNG';
-
-console.log(header);
-
+import './style.css';
 
 class App extends React.Component {
  handleEditorChange = (content, editor) => {
@@ -40,11 +38,61 @@ class App extends React.Component {
   //     );
 
   // doc.output("dataurlnewwindow");
+  var headerArea = document.getElementById("header_doc").outerHTML; 
+  // var contentArea = document.querySelector("iframe").contentWindow;
+  var contentArea = document.getElementsByTagName("iframe")[0];
+  var footerArea = document.getElementById("footer_doc").outerHTML;
+
+  console.log(headerArea);
+  console.log(contentArea);
+  console.log(footerArea);
+
+  // var printArea = headerArea+contentArea+footerArea;
+  // console.log('printArea', printArea);
+
+  var mywindow = window.open('', 'new div');
+  mywindow.document.write('<html><head><title>my div</title>');
+  /*optional stylesheet*/ //mywindow.document.write('<link rel="stylesheet" href="main.css" type="text/css" />');
+  mywindow.document.write('</head><body >');
+  // var headerArea = mywindow.document.createElement("IMG");
+  // headerArea.setAttribute("src", header);
+  // mywindow.document.body.appendChild(headerArea);
+  // mywindow.document.write(headerArea);
+  mywindow.document.body.appendChild(contentArea);
+  // mywindow.document.write(contentArea);
+  // mywindow.document.body.appendChild(footerArea);
+  // mywindow.document.write(footerArea);
+
+  // var contentArea = document.querySelector("iframe").contentWindow;
+  // console.log('contentArea', contentArea);
+  // mywindow.document.body.appendChild(contentArea);
+  // var footerArea = mywindow.document.createElement("IMG");
+  // footerArea.setAttribute("src", footer);
+
+  // mywindow.document.write(printArea);
+  mywindow.document.write('</body></html>');
+
+  mywindow.print();
+  mywindow.close();
 
   // document.querySelector("iframe").contentWindow.print()
-  document.querySelector("#header_doc").contentWindow.print();
+
+  // console.log(printArea)
+
+  // printArea.contentWindow.print();
+  // document.querySelector("#header_doc").contentWindow.print();
 
  }
+
+_renderHeader = () => {
+  // Import result is the URL of your image
+  return <img src={header} alt="Logo" className="hide" id="header_doc" />;
+}
+
+_renderFooter = () => {
+  // Import result is the URL of your image
+  return <img src={footer} alt="Logo" className="hide" id="footer_doc" />;
+}
 
  render() {
    return (
@@ -70,7 +118,9 @@ class App extends React.Component {
          }}
          onEditorChange={this.handleEditorChange}
        />
-       <img id="header_doc" scr={header} />
+
+       { this._renderHeader() }
+       { this._renderFooter() }
        <button onClick={() => this.printToPdf() }>Print</button>
      </Fragment>
    );
